641542255b2fe812821d454072d009e0e3bc87d9
高然 <pangjiping.pjp@alibaba-inc.com>
2026-06-09 14:17:23 +0800

    Revert "feat(egress): add OPENSANDBOX_EGRESS_MITMPROXY_CONFDIR to override confdir"
    This reverts commit 5131006bb9d731f5f7ad87394f85d65b881c9b68.

--- components/egress/pkg/mitmproxy/launch.go
+++ components/egress/pkg/mitmproxy/launch.go
@@ -135,13 +135,8 @@ func Launch(cfg Config) (*Running, error) {
 		Credential: &syscall.Credential{Uid: uid, Gid: gid},
 	}
 	// HOME determines mitm's confdir (~/.mitmproxy) which holds both the CA
-	// and the baked-in config.yaml. Env override takes precedence when a volume
-	// mount shadows the default path.
-	homeEnv := "HOME=" + home
-	if confdir := strings.TrimSpace(os.Getenv(constants.EnvMitmproxyConfdir)); confdir != "" {
-		args = append(args, "--set", "confdir="+confdir)
-	}
-	cmd.Env = append(os.Environ(), homeEnv)
+	// and the baked-in config.yaml.
+	cmd.Env = append(os.Environ(), "HOME="+home)
 
 	if err := cmd.Start(); err != nil {
 		return nil, fmt.Errorf("mitmproxy: start mitmdump: %w", err)
